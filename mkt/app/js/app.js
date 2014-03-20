3f33e8888f98ae235d5da06882a2ddd6b58e2d7e
jmlilly <jmlilly@us.ibm.com>
2014-03-20 16:49:13 -0400

    renamed views.  Added faq and history.  A little better looking overall now.

--- mkt/app/js/app.js
+++ mkt/app/js/app.js
@@ -1,16 +1,28 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+angular.module(
+		'myApp',
+		[ 'ngRoute', 'myApp.filters', 'myApp.services', 'myApp.directives',
+				'myApp.controllers' ]).config(
+		[ '$routeProvider', function($routeProvider) {
+			$routeProvider.when('/hello', {
+				templateUrl : 'partials/hello.html',
+				controller : 'MyCtrl1'
+			});
+			$routeProvider.when('/settings', {
+				templateUrl : 'partials/settings.html',
+				controller : 'MyCtrl1'
+			});
+			$routeProvider.when('/history', {
+				templateUrl : 'partials/history.html',
+				controller : 'MyCtrl2'
+			});
+			$routeProvider.when('/faq', {
+				templateUrl : 'partials/faq.html',
+				controller : 'MyCtrl3'
+			});
+			$routeProvider.otherwise({
+				redirectTo : '/view1'
+			});
+		} ]);
